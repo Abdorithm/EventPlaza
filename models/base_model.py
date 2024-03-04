ab3f1e96db1fa80b602bf73fb5ac1f1159d56974
HazemUsama <hazemosama681@gmail.com>
2024-03-04 19:36:32 +0200

    base model init is done

--- models/base_model.py
+++ models/base_model.py
@@ -1,7 +1,43 @@
 #!/usr/bin/env python3
 """Define the Base Module"""
+from uuid import uuid4
+from sqlalchemy import Column, String, DateTime
+from datetime import datetime, timezone
 
+time = "%Y-%m-%dT%H:%M:%S.%f"
 
 class BaseModel():
     """The base model class for all coming classes"""
+    id = Column(String(60), primary_key=True)
+    created_at = Column(DateTime, default=datetime.now(timezone.utc))
+    updated_at = Column(DateTime, default=datetime.now(timezone.utc))
+
+    def __init__(self, **kwargs):
+        """Initialization of the base model"""
+
+        if kwargs:
+            for key, value in kwargs.items():
+                if key != "__class__":
+                    setattr(self, key, value)
+
+            if 'id' in kwargs:
+                self.id = kwargs['id']
+            else:
+                self.id = str(uuid4())
+            if 'created_at' in kwargs:
+                self.created_at = datetime.strptime(kwargs['created_at'], time)
+            else:
+                self.created_at = datetime.now(timezone.utc)
+            if 'updated_at' in kwargs:
+                self.updated_at = datetime.strptime(kwargs['updated_at'], time)
+            else:
+                self.updated_at = datetime.now(timezone.utc)
+            
+        else:
+            self.id = str(uuid4())
+            self.created_at = datetime.now(timezone.utc)
+            self.updated_at = datetime.now(timezone.utc)
+
+     
+
 
