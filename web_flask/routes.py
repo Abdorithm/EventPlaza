fb147e561b5af0520fe0595e1eaedf5d638e46f7
Abdelrahman Magdi <abdomagdi300@gmail.com>
2024-03-14 23:51:21 +0200

    forms

--- web_flask/routes.py
+++ web_flask/routes.py
@@ -1,7 +1,10 @@
 #!/usr/bin/python3
 """ Starts a Flask Web Application """
-from flask import Flask, render_template
+from flask import Flask, render_template, url_for
+from forms import RegistrationForm, LoginForm
 app = Flask(__name__)
+
+app.config['SECRET_KEY'] = 'd685ddbe85e8fa0e7fb24d5aeb994e8f'
 
 
 @app.route('/', strict_slashes=False)
@@ -12,12 +15,14 @@ def landing():
 @app.route('/login', strict_slashes=False)
 def login():
     """ Renders the log in page """
-    return render_template('log_in.html')
+    form = LoginForm
+    return render_template('log_in.html', form=form)
 
 @app.route('/signup', strict_slashes=False)
 def signup():
     """ Renders the signup page """
-    return render_template('sign_up.html')
+    form = RegistrationForm
+    return render_template('sign_up.html', form=form)
 
 @app.route('/events', strict_slashes=False)
 def events():
